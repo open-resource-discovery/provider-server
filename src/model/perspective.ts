eeccfdf21dd5ec94e5ef2c03bdb0ea5682d77183
Juri Jatschmenow <juri.jatschmenow@raiok.com>
2025-07-09 11:03:23 +0200

    remove comments

--- src/model/perspective.ts
+++ src/model/perspective.ts
@@ -1,15 +1,7 @@
 import { ORDDocument } from "@open-resource-discovery/specification";
 
-/**
- * Perspective type as defined in ORD specification v1.12.0
- */
 export type Perspective = "system-version" | "system-instance" | "system-independent";
 
-/**
- * Default perspective value according to the requirement:
- * "If no `perspective` property is defined in the ORD document, use `system-version` as default"
- * Note: This differs from the ORD spec default which is `system-instance`
- */
 export const DEFAULT_PERSPECTIVE: Perspective = "system-version";
 
 /**
