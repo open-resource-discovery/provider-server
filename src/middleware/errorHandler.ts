78c99a6a0e7cc1c8e54d2a3a1e3b256e64e744b2
Juri Jatschmenow <juri.jatschmenow@raiok.com>
2025-08-15 11:19:36 +0200

    remove redundant check

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -18,12 +18,7 @@ export function errorHandler(err: Error | FastifyError | any, req: FastifyReques
   if (err instanceof BackendError) {
     // The error is already one of our own custom errors, no casting necessary
     castedError = err;
-  } else if (
-    !(err instanceof BackendError) &&
-    err &&
-    err.statusCode === 401 &&
-    err.code === "FST_BASIC_AUTH_MISSING_OR_BAD_AUTHORIZATION_HEADER"
-  ) {
+  } else if (err && err.statusCode === 401 && err.code === "FST_BASIC_AUTH_MISSING_OR_BAD_AUTHORIZATION_HEADER") {
     castedError = new UnauthorizedError("Unauthorized");
   } else if (err instanceof Error) {
     // Handle generic errors we couldn't handle so far
